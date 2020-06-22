e6fbd1c9b08bd4df34df23cf12ef1d54ec5df3e2
Jakub Galinski <thougless@gmail.com>
2020-06-22 23:50:54 +0200

    added data storage logic

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,50 @@
-import '../scss/main.scss';
+import "../scss/main.scss";
+import { registerSW } from "./pwa.js";
+registerSW();
+// localStorage.setItem("Notes", JSON.stringify(NotesArr));
+// JSON.parse(localStorage.getItem("Notes"));
 
-// uncomment the lines below to enable PWA
-// import {registerSW} from './pwa.js';
-// registerSW();
+let counter = document.querySelector(".hydro__counter");
+let addGlass = document.querySelector(".addGlass");
+let delGlass = document.querySelector(".delGlass");
+let glasses = [];
+let newDay = {
+  key: new Date().toISOString().slice(0, 10),
+  value: 0,
+};
+const todayKey = new Date().toISOString().slice(0, 10);
+glasses = JSON.parse(localStorage.getItem("Glasses"));
 
-/* place your code below */
+if (!glasses) {
+  glasses = [newDay];
+  localStorage.setItem("Glasses", JSON.stringify(glasses));
+} else {
+  let found = false;
+  glasses.forEach((day) => {
+    if (day.key == todayKey) {
+      counter.innerHTML = day.value;
+      found = true;
+    }
+  });
+  if (found == false)
+    glasses.push(newDay);
+}
 
-console.log('HELLO 🚀')
+addGlass.addEventListener("click", () => {
+  counter.innerHTML = Number(counter.innerHTML, 10) + 1;
+  setCounter();
+});
+
+delGlass.addEventListener("click", () => {
+  if (counter.innerHTML !== "0") {
+    counter.innerHTML = Number(counter.innerHTML, 10) - 1;
+    setCounter();
+  }
+});
+
+const setCounter = () => {
+  glasses.find((elem) => elem.key == todayKey).value = Number(
+    counter.innerHTML
+  );
+  localStorage.setItem("Glasses", JSON.stringify(glasses));
+};
