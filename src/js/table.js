7ddd241f881d5a8ebc6fa22a03937602e98d72a6
Jakub Galinski <thougless@gmail.com>
2020-07-28 23:16:56 +0200

    feat: installed chart.js, added chart to history, filling chart with data from local storage

--- src/js/table.js
+++ src/js/table.js
@@ -3,7 +3,7 @@ import { registerSW } from "./pwa.js";
 // import "./table.js";
 import "./hamburger.js";
 registerSW();
-
+var Chart = require('chart.js');
 console.log('jaaaajaaa')
 
 let glasses = [];
@@ -24,5 +24,47 @@ const refreshTable = () => {
     valCell.appendChild(document.createTextNode(day.value));
   });
 };
-console.log(historyTable)
 if (historyTable) refreshTable();
+
+let days = glasses.map(day => (day.key))
+let drinkedGlasses = glasses.map(day => (day.value))
+console.log(days)
+console.log(drinkedGlasses)
+var ctx = document.querySelector('#history-chart');
+console.log(myChart)
+var myChart = new Chart(ctx, {
+    type: 'line',
+    data: {
+        labels: days,
+        datasets: [{
+            label: 'Glasses per day',
+            data: drinkedGlasses,
+            // backgroundColor: [
+            //     'rgba(255, 99, 132, 0.2)',
+            //     'rgba(54, 162, 235, 0.2)',
+            //     'rgba(255, 206, 86, 0.2)',
+            //     'rgba(75, 192, 192, 0.2)',
+            //     'rgba(153, 102, 255, 0.2)',
+            //     'rgba(255, 159, 64, 0.2)'
+            // ],
+            // borderColor: [
+            //     'rgba(255, 99, 132, 1)',
+            //     'rgba(54, 162, 235, 1)',
+            //     'rgba(255, 206, 86, 1)',
+            //     'rgba(75, 192, 192, 1)',
+            //     'rgba(153, 102, 255, 1)',
+            //     'rgba(255, 159, 64, 1)'
+            // ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
+            }]
+        }
+    }
+});
