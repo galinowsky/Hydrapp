29d9fa2e47698b0d5f3e0c483bc58b3598c9fb80
Jakub Galinski <thougless@gmail.com>
2020-08-12 11:15:28 +0200

    feat: added height traninstion to app bar, hamburger size and postitioning minor changes

--- src/js/hamburger.js
+++ src/js/hamburger.js
@@ -1,35 +1,28 @@
 let buttonChange = document.querySelector(".js-hamburger");
 let screen = document.documentElement.clientWidth;
 let nav = document.querySelector(".navigation");
-let navHead = document.querySelector('.app-bar')
+let navHead = document.querySelector(".app-bar");
 // let tempChart = document.querySelector('#history-chart');
 
 if (buttonChange) {
   buttonChange.addEventListener("click", () => {
     nav.classList.toggle("navigation__open");
-    navHead.classList.toggle("app-bar__open")
+    navHead.classList.toggle("app-bar__open");
   });
 }
 // console.log(buttonChange)
 // console.log(navHead)
 if (window) {
   window.addEventListener("resize", () => {
-    if ( window.innerWidth >= 520 && nav.classList.contains('navigation__open')){
-       nav.classList.toggle("navigation__open")
-      // nav.setAttribute('display','none')
-    }
     if (
       window.innerWidth >= 520 &&
-      navHead.classList.toggle("app-bar__open")
-      
+      nav.classList.contains("navigation__open")
     ) {
-      // console.log('jajami')
-
-      // tempChart.setAttribute('width', '300')
-      // tempChart.setAttribute('height', '275')
-      //  nav.classList.toggle("navigation__open");
+      nav.classList.toggle("navigation__open");
+      // nav.setAttribute('display','none')
+    }
+    if (window.innerWidth >= 520 && navHead.classList.toggle("app-bar__open")) {
       navHead.classList.toggle("app-bar__open");
-
     }
   });
 }
